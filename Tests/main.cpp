7c4b90bfc92fc56bc957dee6edd3f5cd70adebe3
Willis Pinaud <willispinaud@gmail.com>
2015-12-03 09:29:51 +0100

    add gtests framework

---
+++ Tests/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tests
+//
+//  Created by Willis Pinaud on 03/12/2015.
+//  Copyright © 2015 Centrale. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
